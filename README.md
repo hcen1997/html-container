1539018751f5f56f2e18e3532d287668531d1ecf
hcen <panzetaoyx@qq.com>
2019-11-29 15:07:26 +0800

    create and update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # html-container
 an static-html-container based on java and nginx  prod: 
 
+### root
+nmNLmQtEwGWIt
+
 filename cdci_html/readme.md
+
 
 #### 什么是CD/CI?
 cd current develop 持续开发
@@ -19,18 +23,24 @@ ci current integrate 持续部署
 一个hash 就是一个文件夹
 nginx只是类似一个ftp服务器
 增删改查 hash
- > hash 列表  hash(hash,name)
+ > hash 列表  hash(hash,name)    // 放弃  随着项目多了 有直接查看所有列表的权限非常不好,权限太高了
  > 增加 是一个hash 库    // 增删改是一个 就是替换  没有就创建  有就替换
  > 没有删除
  > 更改是覆盖
- > 查询使用全文搜索 类似`ctrl shift f`   也是一个hash
+ > 查询使用全文搜索 类似`ctrl shift f`   也是一个hash  // 放弃 原因同上
 
 
 ### 预计工期 
-1周
+1周  
 ### 使用技术 
-java nginx jenkins
+java nginx jenkins(放弃,详情见blog) 
 
+### 框架结构
+nginx
+    > frp容器
+java 
+    > 新建容器请求  (new random name dir)
+    > 上传请求 
 
 ### TODO LIST
 [*]  nginx   prod: http://hcen.cc:19000/nmNLmQtEwGWIt/
---
+++ src/main/java/cc/hcen/htmlContainer/HtmlContainerApp.java
@@ -0,0 +1,105 @@
+package cc.hcen.htmlContainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/html-container/api")
+public class HtmlContainerApp implements HandlerInterceptor {
+    private static final int port = 19001;
+    private static final String token = "123456";
+    private String root = "/nginx/html-container ";
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlContainerApp.class);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(port);
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)  {
+        String token = request.getParameter("token");
+        return token != null && token.equals(HtmlContainerApp.token);
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        // 1. get dir name
+        String dir = "null";
+        // run on linux only
+        dir = generateDirName(dir);
+        if (isExists(dir)) {
+            return "exist";
+        } else {
+            // create dir
+            return createDir(dir) ? dir : "create-fail";
+        }
+    }
+
+    private boolean createDir(String dir) {
+        return new File(getPath(dir)).mkdir();
+    }
+
+    private boolean isExists(String dir) {
+        return new File(getPath(dir)).exists();
+    }
+
+    private String getPath(String dir) {
+        return this.root + "/" + dir;
+    }
+
+    private String generateDirName(String dir) {
+        String cmd = "head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''";
+        try {
+            Process proc = Runtime.getRuntime().exec(cmd);
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+            // Read the output from the command
+            dir = stdInput.readLine();
+            stdInput.close();
+            proc.destroy();
+        } catch (IOException e) {
+            System.out.println("urandom error");
+        }
+        return dir;
+    }
+
+    @PostMapping("/update")
+    public String update(@RequestParam String location,
+                         @RequestParam String filename,
+                         @RequestParam MultipartFile file) {
+    // 要保证只能在容器里面上传
+        if(!isInContainer(location)){
+            return "illegal";
+        }
+        //上传 或者覆盖一个文件 transferTo will delete the exist file first
+        String dir = location + "/" + filename;
+        File local = new File(getPath(dir));
+        // 新建
+        try {
+            file.transferTo(local);
+            return dir;
+        } catch (IOException e) {
+            return "error in trans";
+        }
+    }
+
+    private boolean isInContainer(String location) {
+        String[] list = new File(this.root).list((dir, name) -> location.equals(name));
+        return list!=null&& list.length==1;
+    }
+
+}
